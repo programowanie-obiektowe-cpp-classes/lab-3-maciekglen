bf879d9744f0b5edb4d28e573ab01b8709d3b37f
MaciejGlen <glen.maciek@gmail.com>
2025-11-03 21:28:11 +0100

    v1

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,9 +1,36 @@
 #pragma once
 
+#include "Statki.hpp"
 #include "Stocznia.hpp"
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+
+    if (towar == 0) {
+        return 0;
+    }
+
+    int      przewiezionytowar = 0;
+    int      licznikzaglowcow  = 0;
+    Stocznia stocznia{};
+
+    while (przewiezionytowar < towar) {
+
+        Statek* s1            = stocznia();
+        int     chwilowytowar = s1->transportuj();
+
+        przewiezionytowar += chwilowytowar;
+
+        Statek* f = s1;
+
+        Zaglowiec* zaglowiec = dynamic_cast< Zaglowiec* >(f);
+
+        if (zaglowiec) {
+            licznikzaglowcow++;
+        }
+
+        delete s1;
+    }
+
+    return licznikzaglowcow;
 }
